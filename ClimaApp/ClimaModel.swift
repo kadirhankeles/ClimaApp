b04b0e086984420fd1cf67f4ec420544b645dee2
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-03-05 16:47:14 +0300

    Clima App finished

---
+++ ClimaApp/ClimaModel.swift
@@ -0,0 +1,73 @@
+//
+//  ClimaModel.swift
+//  ClimaApp
+//
+//  Created by Kadirhan Keles on 5.03.2023.
+//
+
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let weatherModel = try? JSONDecoder().decode(WeatherModel.self, from: jsonData)
+
+import Foundation
+
+// MARK: - WeatherModel
+struct ClimaModel: Codable {
+    let coord: Coord
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin: Double
+    let tempMax, pressure, humidity: Double
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main, description, icon: String
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+}
--- ClimaApp/ViewController.swift
+++ ClimaApp/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempatureLabel: UILabel!
+    @IBOutlet weak var searchTextfield: UITextField!
+    var sehirAdi = "istanbul"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchWeatherData()
+        searchTextfield.delegate = self
+    }
+    
+    func fetchWeatherData() {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(sehirAdi)&appid=07de6e352cc77e781ec578c937639967&units=metric")!
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+            } else if let data = data {
+                do {
+                    let weatherModel = try JSONDecoder().decode(ClimaModel.self, from: data)
+                    // WeatherModel'e erişerek verileri kullanabilirsiniz
+                    DispatchQueue.main.async {
+                        self.cityLabel.text = weatherModel.name
+                        self.tempatureLabel.text = String(format: "%.1f", weatherModel.main.temp)
+                    }
+                    print("City: \(weatherModel.name), Temperature: \(weatherModel.main.temp), \(weatherModel.id)")
+                } catch {
+                    print("Error: \(error)")
+                }
+            }
+        }
+        task.resume()
+    }
+        
+
+
+    @IBAction func searchButtonClicked(_ sender: UIButton) {
+        searchTextfield.endEditing(true)
+        
+    }
+    
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    //Klavyeyi kapatmamıza yarıyor.
+    searchTextfield.endEditing(true)
+    return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if searchTextfield.text != nil {
+            sehirAdi = searchTextfield.text!
+            fetchWeatherData()
+        }
+    searchTextfield.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    if textField.text != "" {
+    return true
+    } else {
+    textField.placeholder = "Type something"
+    return false
+    }
     }
 
+    }
+    
+    
+    
 
-}
 
